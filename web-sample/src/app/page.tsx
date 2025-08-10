1fbc1f1a173da5dffa3ae3a54284fa5d34846049
masahiroqq <masahiro.secondary@gmail.com>
2025-08-10 12:43:30 +0900

    feat:XSS攻撃可能なコード実装

--- web-sample/src/app/page.tsx
+++ web-sample/src/app/page.tsx
@@ -1,7 +1,33 @@
+"use client";
 
+import { useEffect, useState } from 'react';
+import styles from "@/styles/page/home.module.scss";
 
 export default function Home() {
+  const [userInput, setUserInput] = useState('');
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    setUserInput(params.get('input') || '');
+  }, []);
+
   return (
-    <></>
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <h1 className={styles.title}>オペレーション技術開発部へ ようこそ！</h1>
+
+        <div
+          className={styles.userContent}
+          dangerouslySetInnerHTML={{ __html: userInput }}
+        />
+
+        <img
+          className={styles.logo}
+          src="logo.png"
+          alt="Company Logo"
+        />
+
+      </div>
+    </div>
   );
 }
